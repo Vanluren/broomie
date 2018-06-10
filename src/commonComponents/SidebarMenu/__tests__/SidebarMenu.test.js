1ff00842a78b6531b77d4604efb8e3340d3f016d
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-06-10 23:54:27 +0200

    started testing the sidebar menu

--- src/commonComponents/SidebarMenu/__tests__/SidebarMenu.test.js
+++ src/commonComponents/SidebarMenu/__tests__/SidebarMenu.test.js
@@ -3,16 +3,7 @@ import { shallow } from 'enzyme'
 import SidebarMenu from '../SidebarMenu.container';
 
 describe('SidebarMenu renders correctly', ()=>{
-	let ContainerWrapper;
 	it('Should render without crashing', () => {
 		shallow(<SidebarMenu/>);
 	});
-	
-	beforeEach(()=>{
-		ContainerWrapper = shallow(<SidebarMenu/>);
-	})
-	it('Should render four menupoints', () => {
-		expect(ContainerWrapper.contains(<SidebarLink/>).toBeTruthy());
-	});
-	
 });
