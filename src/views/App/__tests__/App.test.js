edb0cfbb9be5ea37dcbb051731aed62506417480
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-06-13 15:50:59 +0200

    added test for rendering the sidebar menu correctly to App.test.js, added override rules for unused expressions in tests.

--- src/views/App/__tests__/App.test.js
+++ src/views/App/__tests__/App.test.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import Header from '../../../commonComponents/Header/Header.container';
+import SidebarMenu from '../../../commonComponents/SidebarMenu/SidebarMenu.container';
 
-describe('App.Js', ()=>{
-	it('renders without crashing', ()=>{
+describe('App.Js', () => {
+	let container;
+	it('renders without crashing', () => {
 		shallow(<App />);
 	});
-	it('contains a Header-element',()=>{
-		const wrapper = shallow(<App/>);
-// eslint-disable-next-line no-unused-expressions
-		expect(wrapper.contains(<Header/>)).toBeTruthy;
+	beforeEach(() => {
+			container = shallow(<App />)
+	});
+	
+	it('contains a Header-element', () => {
+		expect(container.contains(<Header />)).toBeTruthy;
+	});
+	
+	it('contains a Sidebar-element', () => {
+		expect(container.contains(<SidebarMenu />)).toBeTruthy;
 	});
 });
