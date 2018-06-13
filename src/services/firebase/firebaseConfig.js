ae3eec63775dd5da61820444dc1b3dfdbf77b9e6
Villads V K Nielsen <villadsvalur@gmail.com>
2018-06-13 17:34:18 +0200

    Feature/add firebase (#2)
    * added firebase as a dependency
    
    * added the config file for firebase
    
    * init firebase in App, for use in child components

---
+++ src/services/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	/* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
+	apiKey: "AIzaSyDdRo74ZuKxPvRgUfvyQpLf-fNDuBDGlRw",
+	authDomain: "broomie-549f2.firebaseapp.com",
+	databaseURL: "https://broomie-549f2.firebaseio.com",
+	projectId: "broomie-549f2",
+	storageBucket: "broomie-549f2.appspot.com",
+	messagingSenderId: "506044486203"
+};
