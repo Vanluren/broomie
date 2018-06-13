ded5874f22f9b6410d71cdd75de44b07a8453c8c
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-06-13 17:31:14 +0200

    init firebase in App, for use in child components

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import { Container, Row } from 'reactstrap';
+import firebase from 'firebase/app';
+import { firebaseConfig } from '../../services/firebase/firebaseConfig';
 import Header from '../../commonComponents/Header/Header.container';
 
+firebase.initializeApp(firebaseConfig);
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
-  render() {
-    return (
-    	<Container fluid>
-		    <Row>
-			    <Header/>
-		    </Row>
-	    </Container>
-    );
-  }
+	render() {
+		return (
+			<Container fluid>
+				<Row>
+					<Header />
+				</Row>
+			</Container>
+		);
+	}
 }
 
 export default App;
