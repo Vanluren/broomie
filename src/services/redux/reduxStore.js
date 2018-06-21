6f5cc39004daa5622a5103952174c9adf212cebb
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-06-21 22:52:14 +0200

    created store, added devtools-extension, added thunk to the store, added a basic empty reducer

---
+++ src/services/redux/reduxStore.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const middleware = [thunk];
+const reducer = combineReducers({});
+const store = createStore(
+	reducer, /* preloadedState, */
+	composeWithDevTools(
+		applyMiddleware(...middleware),
+	)
+);
+
+export default store;
