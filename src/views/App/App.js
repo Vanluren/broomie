768b697f6c21060593f70469d916c24e800b1631
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-06-21 21:54:20 +0200

    added routing to app.js

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
-import { Container, Row } from 'reactstrap';
+import { Col, Container, Row } from 'reactstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from '../../commonComponents/Header/Header.container';
 import SidebarMenu from '../../commonComponents/SidebarMenu/SidebarMenu.container';
-
+import Skader from '../SkaderView/Skader.container';
+import Klager from '../KlagerView/KlagerView.container';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
-  render() {
-    return (
-    	<Container fluid>
-		    <Row>
-			    <Header/>
-		    </Row>
-		    <Row>
-			    <SidebarMenu/>
-		    </Row>
-	    </Container>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<Container fluid>
+					<Row>
+						<Header />
+					</Row>
+					<Row>
+						<SidebarMenu />
+						<Switch>
+							<Col md={{ size: 11, offset: 1 }}>
+								<Route
+									path='/skader'
+									component={Skader}
+									exact
+								/>
+								<Route
+									path='/klager'
+									component={Klager}
+								/>
+							</Col>
+						</Switch>
+					</Row>
+				</Container>
+			</Router>
+		);
+	}
 }
 
 export default App;
