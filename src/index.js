435afb50bb64a62851947bf0dc8e316918ee7074
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-08-14 00:56:01 +0200

    Updated Index.js to use the new routing info from Routes.js, renamed home to homeview

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ import './index.css';
 import store from './services/redux/reduxStore';
 import PrivateRoute from './commonComponents/PrivateRoute/PrivateRoute.container';
 import registerServiceWorker from './registerServiceWorker';
-import Home from './views/HomeView/Home.container';
+import HomeView from './views/HomeView/Home.container';
 import LoginView from './views/LoginView/LoginView.container';
 import WithLoadingSpinner from './commonComponents/LoadingSpinner/LoadingSpinner.container';
+import { Routes } from './services/router/router';
 
 ReactDOM.render(
 	<Provider store={store}>
@@ -18,14 +19,14 @@ ReactDOM.render(
 			<WithLoadingSpinner>
 				<Switch>
 					<Route
-						path='/login'
+						path={Routes.LOGIN.path}
 						exact
 						component={LoginView}
 					/>
 					<PrivateRoute>
 						<Route
-							path="/"
-							component={Home}
+							path={Routes.HOME.path}
+							component={HomeView}
 						/>
 					</PrivateRoute>
 				</Switch>
