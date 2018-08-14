6d948fe16fee3f180cc33ee99f0bc88277d18186
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-08-14 18:52:30 +0200

    Removed unused lifecycle methods, refactored the HomeContainer to at functional component, moved the switchRouter to its own component

--- src/views/HomeView/Home.container.js
+++ src/views/HomeView/Home.container.js
@@ -1,96 +1,26 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-//import styled from 'styled-components';
-import { connect } from 'react-redux';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { Col, Container, Row } from 'reactstrap';
 import Header from '../../commonComponents/Header/Header.container';
 import SidebarMenu from '../../commonComponents/SidebarMenu/SidebarMenu.container';
-import { fetchAllTickets } from './ducks/Home.ducks';
-import TicketOverView from '../TicketOverView/TicketOverView.container';
 import TicketTypeNavigation from '../../commonComponents/TicketTypeNav/TicketTypeNavigation.container';
-import { Routes } from '../../services/router/router';
+import WithLoadingSpinner from '../../commonComponents/LoadingSpinner/LoadingSpinner.container';
+import HomeViewRouter from './components/HomeViewRouter';
 
-class Home extends Component {
-	
-	componentDidMount() {
-		// eslint-disable-next-line no-shadow
-		const { isLoggedIn, userData, fetchAllTickets } = this.props;
-		
-		if (isLoggedIn && (userData !== null || userData !== undefined)){
-			fetchAllTickets(userData.visibleDeps);
-		}
-	}
-	
-	render() {
-		const SwitchRouter =
-			<Switch>
-				<Route
-					path={Routes.HOME.path}
-					component={TicketOverView}
-					exact
-				/>
-				<Route
-					path={Routes.SKADER.path}
-					component={Routes.SKADER.component}
-					exact
-				/>
-				<Route
-					path={Routes.KLAGER.path}
-					component={Routes.KLAGER.component}
-					exact
-				/>
-				<Route
-					path={Routes.TICKET.SKADE.path}
-					component={Routes.TICKET.SKADE.component}
-					exact
-				/>
-				<Route
-					path={Routes.TICKET.KLAGE.path}
-					component={Routes.TICKET.KLAGE.component}
-					exact
-				/>
-			</Switch>;
-		
-		return (
-			<Container fluid>
-				<Row>
-					<Header />
-				</Row>
-				<Row>
-					<SidebarMenu />
-					<Col md={{ size: 10, offset: 1 }}>
-						<TicketTypeNavigation />
-						{SwitchRouter}
-					</Col>
-				</Row>
-			</Container>
-		);
-	}
-}
-
-Home.propTypes = {
-	fetchAllTickets: PropTypes.func.isRequired,
-	isLoggedIn: PropTypes.bool.isRequired,
-	// eslint-disable-next-line react/forbid-prop-types
-	userData: PropTypes.object
-};
-Home.defaultProps = {
-	data: null,
-	userData: null || undefined
-};
-
-const mapStateToProps = state => ({
-	isLoggedIn: state.auth.isLoggedIn,
-	userData: state.auth.userData,
-	data: state.data,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-	fetchAllTickets: (depNumber) => dispatch(fetchAllTickets(depNumber))
-});
-
-export default withRouter(connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(Home));
+const HomeView = () => (
+	<Container fluid>
+		<Row>
+			<Header />
+		</Row>
+		<Row>
+			<SidebarMenu />
+			<Col md={{ size: 10, offset: 1 }}>
+				<TicketTypeNavigation />
+				<WithLoadingSpinner>
+					<HomeViewRouter />
+				</WithLoadingSpinner>
+			</Col>
+		</Row>
+	</Container>
+);
+export default withRouter((HomeView));
