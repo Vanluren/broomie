f5f7a1ffbea2b4d696c4fa5aa936b45ed00aff96
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2018-08-18 16:08:03 +0200

    updated the redirectToRequestedPath helper function to check the location.state.from.pathname prop

--- src/services/router/routeHelpers.js
+++ src/services/router/routeHelpers.js
@@ -3,7 +3,7 @@ import { Routes } from './router';
 export const redirectToRequestedPath = (location, history) => {
 	if (location.state === null ||
 	    location.state === undefined ||
-	    location.state.from === '/'){
+	    location.state.from.pathname === '/'){
 		history.push(Routes.HOME.path);
 	} else {
 		history.push(location.state.from);
